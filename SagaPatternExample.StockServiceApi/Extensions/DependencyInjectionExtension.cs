b3fccdae0afa9a3e2158762c03f544816413a9fd
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-03 23:03:11 +0630

    -

--- SagaPatternExample.StockServiceApi/Extensions/DependencyInjectionExtension.cs
+++ SagaPatternExample.StockServiceApi/Extensions/DependencyInjectionExtension.cs
@@ -3,26 +3,25 @@ using SagaPatternExample.Db.AppDbContextModels;
 using SagaPatternExample.StockServiceApi.Config;
 using SagaPatternExample.StockServiceApi.Services;
 
-namespace SagaPatternExample.StockServiceApi.Extensions
+namespace SagaPatternExample.StockServiceApi.Extensions;
+
+public static class DependencyInjectionExtension
 {
-    public static class DependencyInjectionExtension
+    public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration config)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration config)
+        services.AddDbContext<AppDbContext>(opt =>
         {
-            services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(config.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            opt.UseSqlServer(config.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-            services.AddScoped<IStockService, StockService>();
+        services.AddScoped<IStockService, StockService>();
 
-            services.AddHttpContextAccessor();
-            services.Configure<RabbitMqConfiguration>(config.GetSection("RabbitMQ"));
-            services.AddHealthChecks();
+        services.AddHttpContextAccessor();
+        services.Configure<RabbitMqConfiguration>(config.GetSection("RabbitMQ"));
+        services.AddHealthChecks();
 
-            services.AddHostedService<RabbitMQService>();
+        services.AddHostedService<RabbitMQService>();
 
-            return services;
-        }
+        return services;
     }
 }
