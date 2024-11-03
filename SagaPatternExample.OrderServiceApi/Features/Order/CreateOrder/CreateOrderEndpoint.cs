310e0cf451066a794fc5396adcd4c1f2f68f503b
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-03 23:00:00 +0630

    -

--- SagaPatternExample.OrderServiceApi/Features/Order/CreateOrder/CreateOrderEndpoint.cs
+++ SagaPatternExample.OrderServiceApi/Features/Order/CreateOrder/CreateOrderEndpoint.cs
@@ -1,24 +1,23 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace SagaPatternExample.OrderServiceApi.Features.Order.CreateOrder
+namespace SagaPatternExample.OrderServiceApi.Features.Order.CreateOrder;
+
+[Route("api/order")]
+[ApiController]
+public class CreateOrderEndpoint : ControllerBase
 {
-    [Route("api/order")]
-    [ApiController]
-    public class CreateOrderEndpoint : ControllerBase
+    internal readonly ISender _sender;
+
+    public CreateOrderEndpoint(ISender sender)
     {
-        internal readonly ISender _sender;
+        _sender = sender;
+    }
 
-        public CreateOrderEndpoint(ISender sender)
-        {
-            _sender = sender;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateOrderAsync([FromBody] CreateOrderCommand createOrderCommand, CancellationToken cs)
-        {
-            var result = await _sender.Send(createOrderCommand, cs);
-            return Ok(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateOrderAsync([FromBody] CreateOrderCommand createOrderCommand, CancellationToken cs)
+    {
+        var result = await _sender.Send(createOrderCommand, cs);
+        return Ok(result);
     }
 }
