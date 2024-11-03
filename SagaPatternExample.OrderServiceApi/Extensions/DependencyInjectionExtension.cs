b08b5a96a332d6cdc2f1ba9bf925148149d86fe9
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-03 22:55:11 +0630

    -

--- SagaPatternExample.OrderServiceApi/Extensions/DependencyInjectionExtension.cs
+++ SagaPatternExample.OrderServiceApi/Extensions/DependencyInjectionExtension.cs
@@ -3,31 +3,30 @@ using SagaPatternExample.Db.AppDbContextModels;
 using SagaPatternExample.OrderServiceApi.Config;
 using SagaPatternExample.OrderServiceApi.Services;
 
-namespace SagaPatternExample.OrderServiceApi.Extensions
+namespace SagaPatternExample.OrderServiceApi.Extensions;
+
+public static class DependencyInjectionExtension
 {
-    public static class DependencyInjectionExtension
+    public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration config)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration config)
+        services.AddDbContext<AppDbContext>(opt =>
         {
-            services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(config.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            opt.UseSqlServer(config.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-            services.AddMediatR(opt =>
-            {
-                opt.RegisterServicesFromAssembly(typeof(DependencyInjectionExtension).Assembly);
-            });
+        services.AddMediatR(opt =>
+        {
+            opt.RegisterServicesFromAssembly(typeof(DependencyInjectionExtension).Assembly);
+        });
 
-            services.AddScoped<IOrderService, OrderService>();
+        services.AddScoped<IOrderService, OrderService>();
 
-            services.AddHttpContextAccessor();
-            services.Configure<RabbitMqConfiguration>(config.GetSection("RabbitMQ"));
-            services.AddHealthChecks();
+        services.AddHttpContextAccessor();
+        services.Configure<RabbitMqConfiguration>(config.GetSection("RabbitMQ"));
+        services.AddHealthChecks();
 
-            services.AddHostedService<RabbitMQService>();
+        services.AddHostedService<RabbitMQService>();
 
-            return services;
-        }
+        return services;
     }
 }
