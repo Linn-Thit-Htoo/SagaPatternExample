4b83d01630b4bf9268ce1e6a95bdb9d83adff1cb
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-03 23:03:28 +0630

    -

--- SagaPatternExample.StockServiceApi/Models/StockModel.cs
+++ SagaPatternExample.StockServiceApi/Models/StockModel.cs
@@ -1,25 +1,24 @@
-﻿namespace SagaPatternExample.StockServiceApi.Models
+﻿namespace SagaPatternExample.StockServiceApi.Models;
+
+public class StockModel { }
+public class OrderProductRequestModel
 {
-    public class StockModel { }
-    public class OrderProductRequestModel
-    {
-        public string InvoiceNo { get; set; }
-        public List<CreateOrderDetialRequestDTO> OrderDetails { get; set; }
-    }
-    public class CreateOrderDetialRequestDTO
-    {
-        public int ProductId { get; set; }
+    public string InvoiceNo { get; set; }
+    public List<CreateOrderDetialRequestDTO> OrderDetails { get; set; }
+}
+public class CreateOrderDetialRequestDTO
+{
+    public int ProductId { get; set; }
 
-        public int Qty { get; set; }
+    public int Qty { get; set; }
 
-        public decimal Price { get; set; }
+    public decimal Price { get; set; }
 
-        public decimal Subtotal { get; set; }
-    }
+    public decimal Subtotal { get; set; }
+}
 
-    public class ProductRequestModel
-    {
-        public int ProductId { get; set; }
-        public int Qty { get; set; }
-    }
+public class ProductRequestModel
+{
+    public int ProductId { get; set; }
+    public int Qty { get; set; }
 }
