d3c1c619c2d05f14d10fb51049a378080eb37c0a
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-03 22:55:53 +0630

    -

--- SagaPatternExample.OrderServiceApi/Features/Order/CreateOrder/CreateOrderCommand.cs
+++ SagaPatternExample.OrderServiceApi/Features/Order/CreateOrder/CreateOrderCommand.cs
@@ -2,23 +2,22 @@
 using SagaPatternExample.OrderServiceApi.Models;
 using SagaPatternExample.Utils;
 
-namespace SagaPatternExample.OrderServiceApi.Features.Order.CreateOrder
+namespace SagaPatternExample.OrderServiceApi.Features.Order.CreateOrder;
+
+public class CreateOrderCommand : IRequest<Result<OrderModel>>
 {
-    public class CreateOrderCommand : IRequest<Result<OrderModel>>
-    {
-        public int UserId { get; set; }
-        public decimal TotalAmount { get; set; }
-        public List<CreateOrderDetialRequestDTO> OrderDetails { get; set; }
-    }
+    public int UserId { get; set; }
+    public decimal TotalAmount { get; set; }
+    public List<CreateOrderDetialRequestDTO> OrderDetails { get; set; }
+}
 
-    public class CreateOrderDetialRequestDTO
-    {
-        public int ProductId { get; set; }
+public class CreateOrderDetialRequestDTO
+{
+    public int ProductId { get; set; }
 
-        public int Qty { get; set; }
+    public int Qty { get; set; }
 
-        public decimal Price { get; set; }
+    public decimal Price { get; set; }
 
-        public decimal Subtotal { get; set; }
-    }
+    public decimal Subtotal { get; set; }
 }
