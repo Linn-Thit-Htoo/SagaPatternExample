08b12de1ca69696ce1cb0f793c328a6d74647adb
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-03 23:01:54 +0630

    -

--- SagaPatternExample.OrderServiceApi/Services/OrderService.cs
+++ SagaPatternExample.OrderServiceApi/Services/OrderService.cs
@@ -1,31 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SagaPatternExample.Db.AppDbContextModels;
 
-namespace SagaPatternExample.OrderServiceApi.Services
+namespace SagaPatternExample.OrderServiceApi.Services;
+
+public class OrderService : IOrderService
 {
-    public class OrderService : IOrderService
+    internal readonly AppDbContext _context;
+
+    public OrderService(AppDbContext context)
     {
-        internal readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public OrderService(AppDbContext context)
-        {
-            _context = context;
-        }
+    public async Task RollbackOrderAsync(string invoice, CancellationToken cs)
+    {
+        var item = await _context.TbOrders.FirstOrDefaultAsync(x => x.InvoiceNo == invoice, cancellationToken: cs);
+        ArgumentNullException.ThrowIfNull(item, nameof(item));
 
-        public async Task RollbackOrderAsync(string invoice, CancellationToken cs)
-        {
-            var item = await _context.TbOrders.FirstOrDefaultAsync(x => x.InvoiceNo == invoice, cancellationToken: cs);
-            ArgumentNullException.ThrowIfNull(item, nameof(item));
+        var lst = await _context.TbOrderDetails
+            .Where(x => x.InvoiceNo == invoice)
+            .ToListAsync(cs);
+        ArgumentNullException.ThrowIfNull(lst, nameof(lst));
 
-            var lst = await _context.TbOrderDetails
-                .Where(x => x.InvoiceNo == invoice)
-                .ToListAsync(cs);
-            ArgumentNullException.ThrowIfNull(lst, nameof(lst));
+        _context.TbOrders.Remove(item);
+        _context.TbOrderDetails.RemoveRange(lst);
 
-            _context.TbOrders.Remove(item);
-            _context.TbOrderDetails.RemoveRange(lst);
-
-            await _context.SaveChangesAsync(cs);
-        }
+        await _context.SaveChangesAsync(cs);
     }
 }
